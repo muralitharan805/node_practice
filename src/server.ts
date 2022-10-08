387a70e02129d1b67f78c188c6de37333c08b8d7
murali <muralitharan805@gmail.com>
2022-10-08 15:46:23 +0530

    static file

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ var cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
 connectToDatabase()
   .then(() => {
